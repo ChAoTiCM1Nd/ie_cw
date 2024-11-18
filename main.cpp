a9a86c162e1aa1036596cdf2d31bfdf93631752f
Saad <saad_kayani02@hotmail.com>
2024-11-18 12:33:57 +0000

    LCD display now working successfully. Committing library too.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,8 @@
 #include <cstdint>
 #include "LCD_ST7066U.h"
 
+
 LCD lcd(PB_15, PB_14, PB_10, PA_8, PB_2, PB_1); // Adjust pin names as per your hardware
-
-
 
 // Bufferial serial
 BufferedSerial mypc (USBTX, USBRX);
@@ -22,25 +21,21 @@ BufferedSerial mypc (USBTX, USBRX);
 int main()
 {
 
-    lcd.clear();                  // Clear the screen
-    lcd.writeLine("RPM Control", 0); // Write text to the first line
-    lcd.writeLine("Initializing...", 1); // Write text to the second line
+    lcd.writeLine("RPM Control :)", 0); // Write text to the first line
 
     //temporary buffer for read temp
     char temp_data; 
     FILE* mypcFile = fdopen(&mypc, "r+");
 
-    fprintf(mypcFile,"Starting....\n\n");
-
-    fprintf(mypcFile,"\033[0m\033[2J\033[HI2C Searching!\n\n\n"); 
-
-
-
     while (true)
     {
-        lcd.writeLine("Initializing...", 0); // Write text to the second line
-            
+        printf("Starting....\n");
+        lcd.writeLine("Initializing...", 1); // Write text to the second line
 
+        wait_us(500000);
+
+        lcd.clear();
+        
     }
     
 
