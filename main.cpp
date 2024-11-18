72997b4c014fd94c78d4313f7d82ce2cf1fc2f25
Saad <saad_kayani02@hotmail.com>
2024-11-18 14:30:24 +0000

    Committing clean, working version! Ready to merge to main.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//      Note: The code below is to control the PWM fan in a basic fashion. 
+//Basic LCD control!
 
 #include "mbed.h"
 #include <stdio.h>
@@ -13,31 +13,17 @@
 
 LCD lcd(PB_15, PB_14, PB_10, PA_8, PB_2, PB_1); // Adjust pin names as per your hardware
 
-// Bufferial serial
-BufferedSerial mypc (USBTX, USBRX);
-
-
-
 int main()
 {
 
     lcd.writeLine("RPM Control :)", 0); // Write text to the first line
-
-    //temporary buffer for read temp
-    char temp_data; 
-    FILE* mypcFile = fdopen(&mypc, "r+");
-
     while (true)
     {
         printf("Starting....\n");
         lcd.writeLine("Initializing...", 1); // Write text to the second line
-
         wait_us(500000);
-
         lcd.clear();
         
     }
-    
 
-    
 }
